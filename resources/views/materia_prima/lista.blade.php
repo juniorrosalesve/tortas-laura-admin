a2eeb1983e1214f8592aacc1ab7abe43e6a5a29a
Júnior Rosales <74562615+juniorrosalesve@users.noreply.github.com>
2023-11-22 14:39:11 -0500

    Update lista.blade.php

--- resources/views/materia_prima/lista.blade.php
+++ resources/views/materia_prima/lista.blade.php
@@ -179,7 +179,7 @@
                                 </label>
                             </div>
                             <div class="relative z-0 w-full mb-6 group">
-                                <input type="number" value="0" min="1" name="cantidad" id="cantidad" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" />
+                                <input type="number" value="0" min="1" step="0.001" name="cantidad" id="cantidad" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" />
                                 <label for="cantidad" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                     Cantidad
                                 </label>
