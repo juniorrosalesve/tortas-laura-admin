0d60cc08c59487ed8465301489338c14ce1a8d29
Júnior Rosales <74562615+juniorrosalesve@users.noreply.github.com>
2023-11-22 16:21:21 -0500

    Update lista.blade.php

--- resources/views/materia_prima/lista.blade.php
+++ resources/views/materia_prima/lista.blade.php
@@ -97,9 +97,7 @@
                                     <option value="" selected>Elige una opción</option>
                                     <option value="Unidad">Unidad</option>
                                     <option value="Kilogramos">Kilogramos</option>
-                                    <option value="Gramos">Gramos</option>
                                     <option value="Litros">Litros</option>
-                                    <option value="Mililitros">Mililitros</option>
                                 </select>
                                 <label for="unidad" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                     Unidad
